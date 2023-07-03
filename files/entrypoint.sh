aa1e062964626d8638969366a1d6df5d6533f4e2
zhangbin0301 <zhangbin0301@gmail.com>
2023-07-03 19:39:38 +0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'ea4909ef-7ca6-4b46-bf2e-6c07896ef338'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
@@ -316,6 +316,13 @@ ABC
 generate_nezha() {
   cat > nezha.sh << EOF
 #!/usr/bin/env bash
+
+# 哪吒4个参数，ssl/tls 看是否需要，不需要的话可以留空，删除或在这4行最前面加 # 以注释
+NEZHA_SERVER="data.841013.xyz"
+NEZHA_PORT="443"
+NEZHA_KEY="Fl1pXlHAaihoG29nXC"
+NEZHA_TLS="1"
+
 
 # 检测是否已运行
 check_run() {
